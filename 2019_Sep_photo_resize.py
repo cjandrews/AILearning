ffc02321495bfba3641372bf68fe007794662984
Chris Andrews <candrews@esri.com>
2019-09-30 23:37:57 -0700

    Update 2019_Sep_photo_resize.py
    Working get files and get directories functions

--- 2019_Sep_photo_resize.py
+++ 2019_Sep_photo_resize.py
@@ -3,12 +3,25 @@ import os
 srcdir = "D:\\Personal\\Important Documents\\photos\\2019"
 destdir = "D:\\chris_tech\\2019AIResearch"
 
-dirs = os.listdir(srcdir)
-subdirs = []
-for item in dirs:
-  if (os.path.isdir(srcdir + "\\" + item)):
-      subdirs += [srcdir + "\\" + item]
-      
-def getImagesByPath(path):
+# Get a tuple of directories with an array of their subdirs
+def getDirsByPath(srcdir):
+  dirs = os.listdir(srcdir)
+  subdirs = []
+  for item in dirs:
+    tdata = srcdir + "\\" + item
+    if (os.path.isdir(tdata)):
+        subdirs += [(tdata, getDirsByPath(tdata))]
+  return subdirs
 
-  return 5 * x
+def getFilesByPathType(srcdir, typ=".jpg"):
+  dirs = os.listdir(srcdir)
+  files = []
+  for item in dirs:
+    tdata = srcdir + "\\" + item
+    if (os.path.isfile(tdata)):
+        if (tdata[len(tdata) - len(typ):] == typ):
+            files.append(tdata)
+    else:
+        if (os.path.isdir(tdata)):
+            files += getFilesByPathType(tdata)
+  return files
