8e65b63a02e7d69bf1501bc4b3a427a0a84059e8
Chris Andrews <candrews@esri.com>
2019-11-06 20:47:52 -0800

    Update matches_simple2.js

--- protobitmapAI/matches_simple2.js
+++ protobitmapAI/matches_simple2.js
@@ -1,17 +1,17 @@
 const matches = [
-  { ho: {a:1}, aw: {b:1} },
-  { ho: {a:1}, aw: {c:1} },
-  { ho: {a:1}, aw: {e:1} },
-  { ho: {a:1}, aw: {d:1} },
-  { ho: {a:1}, aw: {f:1} },
-  { ho: {c:1}, aw: {b:1} },
-  { ho: {d:1}, aw: {b:1} },
-  { ho: {e:1}, aw: {b:1} },
-  { ho: {f:1}, aw: {b:1} },
-  { ho: {d:1}, aw: {a:1} },
-  { ho: {d:1}, aw: {b:1} },
-  { ho: {b:1}, aw: {c:1} },
-  { ho: {c:1}, aw: {b:1} },
+  { a:1, b:1, home: "a" },
+  { a:1, c:1, home: "a"  },
+  { a:1, d:1, home: "a"  },
+  { a:1, e:1, home: "a"  },
+  { a:1, f:1, home: "a"  },
+  { c:1, b:1, home: "c"  },
+  { d:1, b:1, home: "d"  },
+  { e:1, b:1, home: "e"  },
+  { f:1, b:1, home: "f"  },
+  { d:1, a:1, home: "d"  },
+  { d:1, b:1, home: "d"  },
+  { b:1, c:1, home: "b"  },
+  { c:1, b:1, home: "c"  },
 ];
 
 const matchstats = [
@@ -55,7 +55,7 @@ for (let i = 0; i < matches.length; i++) {
     });
 }
 
-const net = new brain.NeuralNetwork({ hiddenLayers: [5,5] });
+const net = new brain.NeuralNetwork({ hiddenLayers: [5] });
 
 const stats = net.train(trainingData);
 
@@ -64,17 +64,17 @@ console.log(".");
 
 console.log("ec ");
 console.log(net.run(
-  { ho: {e:1}, aw: {c:1} }
+  { e:1, c:1, home: "e"  }
 ));
 console.log("ce ");
 console.log(net.run(
-  { ho: {c:1}, aw: {e:1} }
+  { c:1, e:1, home: "c"  }
 ));
 console.log("bc ");
 console.log(net.run(
-  { ho: {b:1}, aw: {c:1} }
+  { b:1, c:1, home: "b"  }
 ));
 console.log("cb ");
 console.log(net.run(
-  { ho: {c:1}, aw: {b:1} }
+  { c:1, b:1, home: "c"  }
 ));
